f4e3f91d741237f92705eec4c3cdf4c0c8cbb6c5
Hossein Salehi <ho.off.acc@gmail.com>
2026-02-07 13:50:09 +0330

    Rename framework from "ziravel" to "zigavel" for consistency and clarity
    - Updated module and executable names in build.zig to reflect the new framework name.
    - Adjusted package metadata in build.zig.zon to align with the new naming convention.
    - Modified import statements in src/main.zig to utilize the updated framework name, ensuring seamless integration across the application.

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,22 @@
 const std = @import("std");
-const ziravel = @import("ziravel");
-const _router = ziravel.Router;
+const zigavel = @import("zigavel");
+const _router = zigavel.Router;
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    const allocator = ziravel.getDefaultAllocator();
+    const allocator = zigavel.getDefaultAllocator();
     var router = _router.init(allocator);
     try router.get("/hello", handler);
     router.dump();
 
-    var app = ziravel.App.init(router);
+    var app = zigavel.App.init(router);
 
     try app.listen("127.0.0.1", 8080);
 }
 
-fn handler(request: ziravel.Request) void {
+fn handler(request: zigavel.Request) void {
     // _ = request;
     std.debug.print("Hello, world!\n", .{});
     std.debug.print("Request: {s}\n", .{request.url});
-    ziravel.Utils.dump(request);
+    zigavel.Utils.dump(request);
 }
